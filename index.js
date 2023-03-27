16727ec3522011af8b5a5c552b8d6001b1f29f54
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-27 14:48:04 -0700

    added json file and template html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const subredditData = require("./data.json")
+
 
 
 app.set('view engine', "ejs");
@@ -9,8 +11,22 @@ app.set('views', path.join(__dirname, '/views'))
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 })
+app.get('/r/:subreddit', (req,res)=> {
+    const {subreddit} = req.params
+    const data = subredditData[subreddit]
+    console.log(data)
+    res.render("subreddit", {...data})
+})
+
+app.get('/cats', (req,res)=>{
+    const cat = ["biscut", "gravy","shela"];
+    res.render("cats", {cat});
+})
+
+
 app.get('/rand', (req,res)=> {
     const num =  Math.floor((Math.random() * 10) + 1);
+
     res.render('random', {rand: num});   
 })
 
