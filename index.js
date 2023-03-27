c38c5919950b9a977e122727d56ceea5503d61ea
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-26 22:13:08 -0700

    added random number generator for the template /rand

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const path = require(path);
+const path = require('path');
 
 
 app.set('view engine', "ejs");
@@ -9,7 +9,9 @@ app.set('views', path.join(__dirname, '/views'))
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 })
-
+app.get('/rand', (req,res)=> {
+    res.render('random');   
+})
 
 app.listen(3000, ()=> {
     console.log("Listening on port 3000")
