59355d6ea718fb842ae93f4d090f5339e02c0af6
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-28 00:22:09 -0700

    added some stuff

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const subredditData = require("./data.json")
 
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, '/views'))
